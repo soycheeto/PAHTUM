9784d785dd116824cced6fbb409dc83446af2963
soycheeto <trishahansen.ihs@gmail.com>
2024-05-06 12:59:50 +0530

    fixed extra parenthesis on line 9

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 Function Declarations*/
 
 #define SIZE 7
-playMove(char board[SIZE][SIZE], char player, int *x, int *y); .initializeBoard(char board[SIZE][SIZE]);
-printBoard(char board[SIZE][SIZE]));
+playMove(char board[SIZE][SIZE], char player, int *x, int *y);
+initializeBoard(char board[SIZE][SIZE]);
+printBoard(char board[SIZE][SIZE]);
 scoreBoard(char board[SIZE][SIZE], int countX[SIZE], int countO[SIZE]);
 printScores(char board[SIZE][SIZE]);
 playGame(char board[SIZE][SIZE], char player1,char player2);
